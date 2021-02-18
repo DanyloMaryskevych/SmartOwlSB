19242af02078389735830ef3425dc0b348acf5d2
Danylo Maryskevych <danylo.maryskevych@gmail.com>
2021-02-18 10:22:38 +0200

    Initial commit

---
+++ src/main/java/com/spring_boot/smart_owl/SmartOwlApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.smart_owl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartOwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartOwlApplication.class, args);
+	}
+
+}
